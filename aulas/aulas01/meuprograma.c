e35aba2d86c11888f708c4ab690e4d4b5a0f6c59
davifigueredok <25529954-davifigueredok@users.noreply.replit.com>
2023-08-24 00:46:11 +0000

    arquivos da aula 1

---
+++ aulas/aulas01/meuprograma.c
@@ -0,0 +1,15 @@
+/* 
+  Autor: DaviFigueredo  
+  Data: 23/08/2023
+
+  programa que exibe Ola  Pessoal!
+  */
+#include <stdio.h>
+#include <math.h>
+
+// funcao principal
+int main() {
+  printf("Ola pessoal!\n"); 
+  printf("A raiz quadrade de 2 é %f\n"), sqrt(2));
+  return 0;
+}
